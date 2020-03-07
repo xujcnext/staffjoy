402a42619089bbebc2219ed75281fe72712bd846
xujc <jinchang_1988@163.com>
2020-03-07 10:31:58 +0800

    add config

--- mail-api/src/main/java/xyz/staffjoy/mail/MailConstant.java
+++ mail-api/src/main/java/xyz/staffjoy/mail/MailConstant.java
@@ -5,6 +5,6 @@ public class MailConstant {
 
     public static final String FROM_NAME = "Staffjoy";
     public static final String FROM = "staffjoy@dm.staffjoy.xyz";
-    public static final String STAFFJOY_EMAIL_SUFFIX = "@jskillcloud.com";
+    public static final String STAFFJOY_EMAIL_SUFFIX = "@aliyun.com";
     public static final String ALIYUN_REGION_ID = "cn-hangzhou";
 }
